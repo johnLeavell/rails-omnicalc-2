d7fb1a12abfb71399a9e32e0bd310df8c1f3f86f
johnLeavell <johnleavell@gmail.com>
2023-08-30 16:26:57 -0500

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "addition", :action => "show_addition_form" })
+  get("/", { controller: "addition", action: "show_addition_form" })
 
-  get("/ad", { :controller => "addition", :action => "show_addition_form" })
-  get("/wizard_add", { :controller => "addition", :action => "add_these" })
+  get("/add", { controller: "addition", action: "show_addition_form" })
 
-  get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
-  get("", { :controller => "subtraction", :action => "subtract" })
+  get("/wizard_addition", { controller: "addition", action: "add_these" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
-  get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
+  get("/subtract", { controller: "subtraction", action: "show_sub_form" })
 
-  get("/divide", { :controller => "division", :action => "show_division_form" })
-  get("/wizard_div", { :controller => "div", :action => "divide_these" })
+  get("/wizard_subtract", { controller: "subtraction", action: "subtract" })
+
+  get("/multiply", { controller: "multiplication", action: "show_multiplication_form" })
+
+  get("/wizard_multiply", { controller: "multiplication", action: "multiply" })
+
+  get("/divide", { controller: "division", action: "show_division_form" })
+  
+  get("/wizard_divide", { controller: "division", action: "divide_these" })
 
 end
